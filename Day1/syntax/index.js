302c0865c1565843fc486c3c3ea0233eb5941429
nikhilsshriffle <nikhils@shriffle.com>
2023-03-14 12:41:56 +0530

    First Commit

---
+++ Day1/syntax/index.js
@@ -0,0 +1,8 @@
+// In JavaScript we create a variable with help of var let const 
+var x;
+let y;
+x = parseInt(prompt("Enter a first number"));
+y = parseInt(prompt("Enter a second number"));
+const z = x + y;    
+alert(`Sum of Number ${z}`)
+console.log("z",z)
