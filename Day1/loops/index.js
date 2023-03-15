ec6eaeee6677ccd678207ce6e7c3bad2185c60fe
nikhilsshriffle <nikhils@shriffle.com>
2023-03-15 11:33:07 +0530

    Added Loops file

--- Day1/loops/index.js
+++ Day1/loops/index.js
@@ -4,6 +4,42 @@ const arr = [1,2,3,4,5,6,7,8,9,10]
 
 for (let i=0; i<arr.length; i++) {
   if(arr[i]%2 ===0) {
-    console.log("Even No",arr[i]);
+    console.log("Even No  normal for loop",arr[i]);
   }
 }
+console.log('\n')
+
+for (let i in arr) {
+  if(arr[i]%2 ===0) {
+    console.log("Even No for in",arr[i]);
+  }
+}
+console.log('\n')
+
+for (let i of arr) {
+  if(i%2 ===0) {
+    console.log("Even No for of",i);
+  }
+}
+
+console.log('\n')
+
+let i = 0;
+while (i<arr.length) {
+  if(arr[i]%2 ===0) {
+    console.log("Even No while loop",arr[i]);
+  }
+  i++;
+}
+
+console.log('\n')
+
+do {
+  if(arr[i]%2 ===0) {
+    console.log("Even No do while loop",arr[i]);
+  }  
+  i++;
+}
+while (i < arr.length);  
+
+
